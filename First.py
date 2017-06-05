b923ffdca1885a969892e0d5703acb3885acc97e
betwulf <betwulf@hotmail.com>
2017-06-05 14:08:33 +0000

    variables and assignment

--- First.py
+++ First.py
@@ -13,6 +13,30 @@ print("sess:", sess.run(node3))
 
 a = tf.placeholder(tf.float32)
 b = tf.placeholder(tf.float32)
+aplusb = a + b
 
-print(sess.run(a+b, {a: 3.5, b: 4.5}))
-print(sess.run(a+b, {a: [3, 2], b: [4, 5]}))
+print(sess.run(a + b, {a: 3.5, b: 4.5}))
+print(sess.run(aplusb, {a: [3, 2], b: [4, 5]}))
+
+tripleab = 3 * aplusb
+
+print(sess.run(tripleab, {a: [3, 2], b: [4, 5]}))
+
+m = tf.Variable([.3], tf.float32)
+b = tf.Variable([-.3], tf.float32)
+x = tf.placeholder(tf.float32)
+linear_model = m*x + b
+
+init = tf.global_variables_initializer()
+sess.run(init)
+print(sess.run(linear_model, {x: [1, 2, 3, 4]}))
+
+y = tf.placeholder(tf.float32)
+
+errorsquared = tf.reduce_sum(tf.square(linear_model - y))
+print(sess.run(errorsquared, {x: [1, 2, 3, 4], y:[0, -1, -2, -3]}))
+fixm = tf.assign(m, [-1])
+fixb = tf.assign(b, [1])
+sess.run([fixm, fixb])
+print(sess.run(errorsquared, {x: [1, 2, 3, 4], y:[0, -1, -2, -3]}))
+
